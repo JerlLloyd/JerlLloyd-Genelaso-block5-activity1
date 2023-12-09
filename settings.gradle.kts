9d72dd27bb38c1c8411865edb13fca0309004166
Jerl Lloyd <genelasojerlloyd@gmail.com>
2023-12-09 23:01:36 +0800

    Project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JerlLloydGenelaso-Block5-Quiz"
+include(":app")
